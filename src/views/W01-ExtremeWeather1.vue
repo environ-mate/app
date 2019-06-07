4108dbf4afcfe9b468b30af1d2b43d056f28b63c
Bernhard Janetzki <boerni@gmail.com>
2019-06-07 10:48:17 +0200

    colorized + i18n Extremewaether

--- src/views/W01-ExtremeWeather1.vue
+++ src/views/W01-ExtremeWeather1.vue
@@ -2,32 +2,22 @@
 {
   "de": {
     "title": "Was geschieht mit unserem Wetter?",
-    "desc_1": "Den größten Anteil am Treibhauseffekt hat das Gas CO₂. Der Autoverkehr und die Verbrennung fossiler Brennstoffe (Gas, Erdöl, Kohle) verursachen die meisten CO₂-Emissionen (Ausstoß des Gases in die Umwelt).",
-    "desc_2": "Wenn Treibhausgase einmal freigesetzt sind, bleiben sie unterschiedlich lange in der Atmosphäre - CO₂ zum Beispiel bis zu 120 Jahre.",
-    "desc_3": "Daher können wir die Effekte auch mit einem unmittelbaren Stopp der Emissionen nicht sofort umkehren, aber zumindest dafür sorgen, dass sich die Situation nicht verschlimmert.",
-    "bullet_desc": "Sie halten unterschiedlich stark die Sonnenwärme zurück auf der Erde: SF₆ z.B. 23.500-fach stärker als CO₂",
-    "vis_title": "Treibhausgas-Emissionen {year} in deinem Heimatland {homeTown}",
-    "vis_legend_agriculture": "Landwirtschaft",
-    "vis_legend_energy": "Energiesektor",
-    "vis_legend_waste": "Abfall",
-    "vis_legend_transport": "Verkehr",
-    "vis_legend_industry": "Industrie",
-    "vis_legend_other": "Sonstige",
+    "vis_title": "TODO",
+    "vis_legend_storm": "Sturm",
+    "vis_legend_wildfire": "Flächenbrand",
+    "vis_legend_flood": "Überschwemmung",
+    "vis_legend_drought": "Dürre",
+    "desc": "TODO",
     "next_btn": "Übersicht"
   },
   "en": {
-    "title": "What are greenhouse gases and where do they come from?",
-    "desc_1": "The biggest contributor to the greenhouse effect is CO₂ gas. Car traffic and the burning of fossil fuels (gas, oil, coal) cause most of the CO₂ emissions (emissions of the gas into the environment).",
-    "desc_2": "Once greenhouse gases are released their stay in the atmosphere varies - CO₂ for example up to 120 years.",
-    "desc_3": "Therefore, we can not immediately reverse the effects with a sudden stop of emissions, but at least make sure that the situation does not get worse.",
-    "bullet_desc": "They can hold back the sun's heat on earth with varying degrees of intensity: SF₆ e.g. 23.500 times stronger than CO₂",
-    "vis_title": "Greenhouse gas emissions {year} in your country {homeTown}",
-    "vis_legend_agriculture": "agriculture",
-    "vis_legend_energy": "energy sector",
-    "vis_legend_waste": "waste",
-    "vis_legend_transport": "transport",
-    "vis_legend_industry": "industry",
-    "vis_legend_other": "other",
+    "title": "",
+    "vis_title": "",
+    "vis_legend_storm": "Storm",
+    "vis_legend_wildfire": "Wildfire",
+    "vis_legend_flood": "Flood",
+    "vis_legend_drought": "Drought",
+    "desc": "",
     "next_btn": "Overview"
   }
 }
@@ -47,9 +37,7 @@
       <div class="modal-body">
         <div class="columns">
           <div class="column col-4">
-            <p>{{ $t('desc_1') }}</p>
-            <p>{{ $t('desc_2') }}</p>
-            <p>{{ $t('desc_3') }}</p>
+            <p>{{ $t('desc') }}</p>
           </div>
           <div class="column col-1 flex">
             <img class="img-responsive flex-end" v-bind:src="'/assets/wimmel/' + this.$parent.$data.tutor.image"/>
@@ -104,7 +92,10 @@ export default {
         url: '/data/extreme_weather_occurences.csv',
         x: 'year',
         names: {
-          'storm.occurrence': this.$t('storm.occurrence'),
+          'storm.occurrence': this.$t('vis_legend_storm'),
+          'wildfire.occurrence': this.$t('vis_legend_wildfire'),
+          'flood.occurrence': this.$t('vis_legend_flood'),
+          'drought.occurrence': this.$t('vis_legend_drought'),
         },
         type: 'area',
         colors: {
