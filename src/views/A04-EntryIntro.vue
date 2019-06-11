4a0beeb450b393a5b1b2897f152699528b8cf4a5
matthias.boeck <matthias.boeck@feld-m.de>
2019-06-11 17:47:39 +0200

    Update on axis description

--- src/views/A04-EntryIntro.vue
+++ src/views/A04-EntryIntro.vue
@@ -4,8 +4,8 @@
     "title": "Klimawandel, was ist das eigentlich?",
     "intro_1": "Du hast sicher schon mal vom Klimawandel gehört. Was hat es damit auf sich?",
     "intro_2": "In den letzten Jahrzehnten hat sich die Durchschnittstemperatur auf unserer Erde drastisch erhöht. 1-2 Grad hören sich dabei wenig an, haben aber einen großen Effekt auf unsere Umwelt.",
-    "chart_degress_desc": "Schaut man sich die Durchschnittstemperaturen in Europa im Vergleich zur Zeit vor 1900 an, sieht man wie gerade die letzten Jahre immer wärmer wurden:",
-    "chart_degress": "Durchschnittstemperaturen",
+    "chart_degress_desc": "Schaut man sich die Abweichung der Durchschnittstemperaturen in Europa im Vergleich zur Zeit vor 1900 an, sieht man wie gerade die letzten Jahre immer wärmer wurden:",
+    "chart_degress": "Temperaturabweichung vom Durchschnitt vor der Industrialisierung (1900)",
     "next_desc": "Aber wieso hat sich die Erde auf einmal erwärmt?",
     "next_next_desc": "Das erkläre ich dir auf den nächsten Seiten.",
     "next_btn": "Start Erklärung Klimawandel"
@@ -15,7 +15,7 @@
     "intro_1": "You've likely heard of climate change. So, what's it all about?",
     "intro_2": "In recent decades the average temperature of earth has dramatically increased.  1-2 degrees doesn't sound like much, but has a very high impact on our environment.",
     "chart_degress_desc": "If you take a look at the average temperatures in Europe and compare it to before 1900, you can see it is becoming hotter and hotter:",
-    "chart_degress": "Average temperatures",
+    "chart_degress": "Temperature deviation from pre industrial average (1900)",
     "next_desc": "But why the increase in temperatures on earth?",
     "next_next_desc": "I'll explain that to you over the next pages.",
     "next_btn": "Start explanation climate change"
