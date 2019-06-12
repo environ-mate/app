17c291b92de0f0707375ea44ec7a8235b226a54b
Bernhard Janetzki <boerni@gmail.com>
2019-06-12 21:52:06 +0200

    leftovers

--- src/views/Z04-ActRandom.vue
+++ src/views/Z04-ActRandom.vue
@@ -33,7 +33,7 @@
            aria-label="Close"></a>
 
         <div class="modal-title h4 flex-centered">
-           💪 {{ $t("title") }}
+           {{ $t("title") }}
         </div>
       </div>
       <div class="modal-body">
@@ -44,7 +44,7 @@
           </div>
           <div class="column col-7">
             <ul>
-              <li>🐖🐄 {{ $t('act_1') }}</li>
+              <li>{{ $t('act_1') }}</li>
               <li>{{ $t('act_2') }}</li>
               <li>{{ $t('act_3') }}</li>
               <li>{{ $t('act_4') }}</li>
