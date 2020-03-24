4ef80d751bed63bfcbb5c71f467bb2dcbb599de1
alexvmt <alexandermerdiantarko@gmail.com>
2020-03-24 21:52:33 +0100

    started to update ghg emissions data (2019)

--- data/data_processing_NEW_WIP.R
+++ data/data_processing_NEW_WIP.R
@@ -33,8 +33,12 @@ library(tidyverse)
 # move to ../app/data/raw_data/temperature/
 
 # load, process and write data
+col_names <- c("year",
+               "temperature.deviation.degree.celcius",
+               "type")
+
 read_csv("data/raw_data/temperature/european_land_temperature_deviations_annual_raw.csv",
-         col_names = c("year", "temperature.deviation.degree.celcius", "type"),
+         col_names = col_names,
          col_types = "i--n---c-",
          skip = 1) %>% 
   filter(type == "European annual") %>% 
@@ -49,6 +53,129 @@ read_csv("data/raw_data/temperature/european_land_temperature_deviations_annual_
 
 ##### Emissions #####
 
+### greenhouse gas emissions projections europe
+# download latest and checked projections as zipped csv from https://www.eea.europa.eu/data-and-maps/data/greenhouse-gas-emission-projections-for-6
+# extract and rename file to ghg_emissions_projections_europe_raw.csv
+# move to ../app/data/raw_data/ghg_emissions/
+
+# load and process data (select most recent full year, categories and scenario)
+col_names <- c("country.code",
+               "year",
+               "category",
+               "category.name",
+               "scenario",
+               "gas",
+               "ghg.emissions.kilo.tonnes.co2e")
+
+ghg_emissions_projections_europe_raw <- read_csv("data/raw_data/ghg_emissions/ghg_emissions_projections_europe_raw.csv",
+                                                 col_names = col_names,
+                                                 col_types = "ci-cccc--n",
+                                                 skip = 1) %>% 
+  filter(year == 2019 &
+           category.name == "Total w.out LULUCF" &
+           scenario == "WEM" &
+           gas == "Total GHGs (ktCO2e)") %>% 
+  mutate(country.name = case_when(
+    country.code == "AT" ~ "Austria",
+    country.code == "BE" ~ "Belgium",
+    country.code == "BG" ~ "Bulgaria",
+    country.code == "CH" ~ "Switzerland",
+    country.code == "CY" ~ "Cyprus",
+    country.code == "CZ" ~ "Czech Republic",
+    country.code == "DE" ~ "Germany",
+    country.code == "DK" ~ "Denmark",
+    country.code == "EE" ~ "Estonia",
+    country.code == "ES" ~ "Spain",
+    country.code == "EU" ~ "EU28",
+    country.code == "FI" ~ "Finland",
+    country.code == "FR" ~ "France",
+    country.code == "GB" ~ "UK",
+    country.code == "GR" ~ "Greece",
+    country.code == "HR" ~ "Croatia",
+    country.code == "HU" ~ "Hungary",
+    country.code == "IE" ~ "Ireland",
+    country.code == "IS" ~ "Iceland",
+    country.code == "IT" ~ "Italy",
+    country.code == "LT" ~ "Lithuania",
+    country.code == "LU" ~ "Luxembourgh",
+    country.code == "LV" ~ "Latvia",
+    country.code == "MT" ~ "Malta",
+    country.code == "NL" ~ "Netherlands",
+    country.code == "NO" ~ "Norway",
+    country.code == "PL" ~ "Poland",
+    country.code == "PT" ~ "Portugal",
+    country.code == "RO" ~ "Romania",
+    country.code == "SE" ~ "Sweden",
+    country.code == "SI" ~ "Slovenia",
+    country.code == "SK" ~ "Slovakia",
+    TRUE ~ NA_character_)) %>% 
+  mutate(ghg.emissions.kilo.tonnes.co2e = round(ghg.emissions.kilo.tonnes.co2e / 1000, 1)) %>% 
+  select(country.name,
+         year,
+         category,
+         category.name,
+         ghg.emissions.kilo.tonnes.co2e)
+
+### todo
+# select correct sectors (agriculture, energy, industry, total, transport, waste and other)
+# compute per capita emissions in tonnes
+# compute ghg emissions in mio tonnes per sector for each country
+# compute national share ghg emissions per sector for each country
+# compute Eu28 share ghg emissions per sector for each country
+# compute rank based on total emissions in mio tonnes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# Plot confidence intervals for future projections
+#
+#
+library(ggplot2)
+data <- read.csv("public/data/future-greenhouse-gas-emission-scenarios.csv", header=T)
+#data[,-c(1,2)] <- apply(data[,-c(1,2)], 2, function(x) ifelse(x<0, 0, x))
+
+
+# MEDIAN of last entry for label positioning
+# no climate policies: 4.1-4.8?C - 128.9
+# current policies: 3.1-3.7?C    - 62.75
+# pledges: (2.6-3.2?C)           - 41.67
+# pathway: 2?C                   - 2.97
+# pathway: 1.5?C                 - -5.46
+
+
+# plot
+alpha = 0.6
+image <- ggplot(data=data)+  
+  geom_line(aes(x = Year, y = Historic..billion.tonnes.CO..equivalent.), color = "black")+
+  geom_ribbon(aes(x = Year, ymin= No.climate.policies..low...billion.tonnes.CO..equivalent., ymax =  No.climate.policies..high...billion.tonnes.CO..equivalent.), fill= "#fb6a4a", alpha = alpha)+  
+  geom_ribbon(aes(x = Year, ymin= Current.policies..low...billion.tonnes.CO..equivalent., ymax =  Current.policies..high...billion.tonnes.CO..equivalent.), fill= "#9e9ac8", alpha = alpha)+ 
+  geom_ribbon(aes(x = Year, ymin= Pledges..low...billion.tonnes.CO..equivalent., ymax =  Pledges..high...billion.tonnes.CO..equivalent.), fill= "#fed976", alpha = alpha)+ 
+  geom_ribbon(aes(x = Year, ymin= X2C.pathways..low...billion.tonnes.CO..equivalent., ymax =  X2C.pathways..high...billion.tonnes.CO..equivalent.), fill= "#74c476", alpha = alpha)+ 
+  geom_ribbon(aes(x = Year, ymin= X1.5C.pathways..low...billion.tonnes.CO..equivalent., ymax =  X1.5C.pathways..high...billion.tonnes.CO..equivalent.), fill= "#67a9cf", alpha = alpha)+ 
+  ylim(-10,200) + xlim(1990,2100)+
+  theme_bw()+ 
+  theme(axis.text=element_text(size=14))+
+  #theme (panel.border = element_blank()) + geom_hline(yintercept=0) + geom_vline(xintercept=1990) + 
+  labs(y="Gt CO2", x="t")+
+  scale_y_continuous(sec.axis=dup_axis(name=" ", breaks=c(-5.46, 2.97,41.67,62.75,128.9), labels=c("1.5?C","2?C","2.6-3.2?C","3.1-3.7?C","4.1-4.8?C")))
+
+#save the plot as png
+ggsave(file="public/assets/emission_projection_2100.png", plot=image, width=12, height=6)  
+
 
 
 ##################################################
@@ -58,7 +185,7 @@ read_csv("data/raw_data/temperature/european_land_temperature_deviations_annual_
 ##### Extreme weather #####
 
 ### extreme weather occurences europe
-# download csv from https://www.emdat.be/emdat_db/
+# sign up at and download csv from https://www.emdat.be/emdat_db/
 # period: 1900 to most recent full year
 # location: continent -> europe
 # disasters classification: natural
@@ -67,8 +194,15 @@ read_csv("data/raw_data/temperature/european_land_temperature_deviations_annual_
 # move to ../app/data/raw_data/extreme_weather/
 
 # load and process data (include climate related disasters only and exlude sparse pre-1960 data)
+col_names <- c("country.name",
+               "disaster.type",
+               "year",
+               "occurrence",
+               "total.deaths",
+               "total.damage.thousand.dollars")
+
 extreme_weather_occurrences_europe_since_1960 <- read_csv("data/raw_data/extreme_weather/extreme_weather_occurrences_europe_raw.csv",
-                                                          col_names = c("country.name", "disaster.type", "year", "occurrence", "total.deaths", "total.damage.thousand.dollars"),
+                                                          col_names = col_names,
                                                           col_types = "-cciii----n",
                                                           skip = 2) %>% 
   head(-2) %>% 
